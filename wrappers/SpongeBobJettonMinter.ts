890fd3e5a2264d68d1be6650d0c4b7c8ecdff761
leven <leven890619@gmail.com>
2024-07-29 10:52:40 +0800

    commit wrap ts

--- wrappers/SpongeBobJettonMinter.ts
+++ wrappers/SpongeBobJettonMinter.ts
@@ -1,9 +1,40 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, toNano } from '@ton/core';
+import {Op} from './JettonConstants';
 
-export type SpongeBobJettonMinterConfig = {};
+export type JettonMinterContent = {
+    uri: string
+};
+
+export type SpongeBobJettonMinterConfig = {
+    max_supply: number;
+    admin_address: Address,
+    airdrop_contract_address: Address,
+    public_sale_contract_address: Address,
+    team_contract_address: Address,
+    treasury_contract_address: Address,
+    jetton_wallet_code: Cell,
+    jetton_content: Cell | JettonMinterContent
+};
 
 export function spongeBobJettonMinterConfigToCell(config: SpongeBobJettonMinterConfig): Cell {
-    return beginCell().endCell();
+    const content = config.jetton_content instanceof Cell ? config.jetton_content : jettonContentToCell(config.jetton_content);
+    return beginCell()
+            .storeCoins(0)
+            .storeCoins(config.max_supply)
+            .storeAddress(config.admin_address)
+            .storeAddress(config.airdrop_contract_address)
+            .storeAddress(config.public_sale_contract_address)
+            .storeAddress(config.team_contract_address)
+            .storeAddress(config.treasury_contract_address)
+            .storeRef(config.jetton_wallet_code)
+            .storeRef(content)
+            .endCell();
+}
+
+export function jettonContentToCell(content: JettonMinterContent) {
+    return beginCell()
+        .storeStringRefTail(content.uri) //Snake logic under the hood
+        .endCell();
 }
 
 export class SpongeBobJettonMinter implements Contract {
@@ -23,7 +54,133 @@ export class SpongeBobJettonMinter implements Contract {
         await provider.internal(via, {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
+            body: beginCell().storeUint(Op.top_up, 32).storeUint(0, 64).endCell(),
         });
+    }
+
+    static mintToClaimAirdropMessage(to: Address, jetton_amount: bigint, from?: Address | null, response?: Address | null, customPayload?: Cell | null, forward_ton_amount: bigint = 0n, total_ton_amount: bigint = 0n) {
+        const mintToAirdropMsg = beginCell().storeUint(Op.internal_transfer, 32)
+            .storeUint(0, 64)
+            .storeCoins(jetton_amount)
+            .storeAddress(from)
+            .storeAddress(response)
+            .storeCoins(forward_ton_amount)
+            .storeMaybeRef(customPayload)
+            .endCell();
+        return beginCell().storeUint(Op.mint_to_airdrop_contract, 32).storeUint(0, 64) // op, queryId
+            .storeAddress(to)
+            .storeCoins(total_ton_amount)
+            .storeRef(mintToAirdropMsg)
+            .endCell();
+    }
+
+    async sendMintToClaimAirdropMessage(provider: ContractProvider,
+                   via: Sender,
+                   to: Address,
+                   jetton_amount: bigint,
+                   from?: Address | null,
+                   response_addr?: Address | null,
+                   customPayload?: Cell | null,
+                   forward_ton_amount: bigint = toNano('0.05'), total_ton_amount: bigint = toNano('0.1')) {
+        await provider.internal(via, {
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: SpongeBobJettonMinter.mintToClaimAirdropMessage(to, jetton_amount, from, response_addr, customPayload, forward_ton_amount, total_ton_amount),
+            value: total_ton_amount,
+        });
+    }
+
+    static topUpMessage() {
+        return beginCell().storeUint(Op.top_up, 32).storeUint(0, 64) // op, queryId
+            .endCell();
+    }
+
+    async sendTopUp(provider: ContractProvider, via: Sender, value: bigint = toNano('0.1')) {
+        await provider.internal(via, {
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: SpongeBobJettonMinter.topUpMessage(),
+            value: value,
+        });
+    }
+
+    static changeAdminMessage(newOwner: Address) {
+        return beginCell().storeUint(Op.change_admin, 32).storeUint(0, 64) // op, queryId
+            .storeAddress(newOwner)
+            .endCell();
+    }
+
+    async sendChangeAdmin(provider: ContractProvider, via: Sender, newOwner: Address) {
+        await provider.internal(via, {
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: SpongeBobJettonMinter.changeAdminMessage(newOwner),
+            value: toNano("0.1"),
+        });
+    }
+
+    static changeContentMessage(content: Cell | JettonMinterContent) {
+        const contentString = content instanceof Cell ? content.beginParse().loadStringTail() : content.uri;
+        return beginCell().storeUint(Op.change_metadata_url, 32).storeUint(0, 64) // op, queryId
+            .storeStringTail(contentString)
+            .endCell();
+    }
+
+    async sendChangeContent(provider: ContractProvider, via: Sender, content: Cell | JettonMinterContent) {
+        await provider.internal(via, {
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: SpongeBobJettonMinter.changeContentMessage(content),
+            value: toNano("0.1"),
+        });
+    }
+
+    static upgradeMessage(new_code: Cell, new_data: Cell, query_id: bigint | number = 0) {
+        return beginCell().storeUint(Op.upgrade, 32).storeUint(query_id, 64)
+            .storeRef(new_data)
+            .storeRef(new_code)
+            .endCell();
+    }
+
+    async sendUpgrade(provider: ContractProvider, via: Sender, new_code: Cell, new_data: Cell, value: bigint = toNano('0.1'), query_id: bigint | number = 0) {
+        await provider.internal(via, {
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: SpongeBobJettonMinter.upgradeMessage(new_code, new_data, query_id),
+            value
+        });
+    }
+
+    async getWalletAddress(provider: ContractProvider, owner: Address): Promise<Address> {
+        const res = await provider.get('get_wallet_address', [{
+            type: 'slice',
+            cell: beginCell().storeAddress(owner).endCell()
+        }])
+        return res.stack.readAddress()
+    }
+
+    async getJettonData(provider: ContractProvider) {
+        let res = await provider.get('get_jetton_data', []);
+        let totalSupply = res.stack.readBigNumber();
+        let mintable = res.stack.readBoolean();
+        let adminAddress = res.stack.readAddress();
+        let content = res.stack.readCell();
+        let walletCode = res.stack.readCell();
+        return {
+            totalSupply,
+            adminAddress,
+            content,
+            walletCode,
+        };
+    }
+
+    async getTotalSupply(provider: ContractProvider) {
+        let res = await this.getJettonData(provider);
+        return res.totalSupply;
+    }
+
+    async getAdminAddress(provider: ContractProvider) {
+        let res = await this.getJettonData(provider);
+        return res.adminAddress;
+    }
+
+    async getContent(provider: ContractProvider) {
+        let res = await this.getJettonData(provider);
+        return res.content;
     }
 }
